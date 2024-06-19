9f385c763a8bd3a7bebccb067e3f9f8c879eb120
alphak3y <alphak3y@protonmail.com>
2024-06-19 06:36:34 -0600

    main rs

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -8,18 +8,18 @@ fn main() {
     println!("Hello, world!");
 
     // Create a provider pointing to the testnet.
-    let provider = Provider::connect("testnet.fuel.network");
+    let provider = Provider::connect("testnet.fuel.network").await;
 
     // Setup a private key
     let secret = SecretKey::from_str(
         "830499a7b3ed234ab617ad6c402c623d0b4318cc4737059c28169e239a25101d",
     );
 
-    let wallet = WalletUnlocked::new_from_private_key(secret.expect("INVALID SECRET"), Some(provider.await.expect("INVALID PROVIDER")));
+    // let wallet = WalletUnlocked::new_from_private_key(secret.expect("INVALID SECRET"), Some(provider.await.expect("INVALID PROVIDER")));
 
-    // Get the wallet address. Used later with the faucet
-    dbg!(wallet.address().to_string());
+    // // Get the wallet address. Used later with the faucet
+    // dbg!(wallet.address().to_string());
 
-    println!("Wallet address:");
-    println!("{}", wallet.address().to_string());
+    // println!("Wallet address:");
+    // println!("{}", wallet.address().to_string());
 }

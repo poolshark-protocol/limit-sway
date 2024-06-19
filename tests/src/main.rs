8bd14bdda4079e4562df24f8850d9618fc4abbc5
alphak3y <alphak3y@protonmail.com>
2024-06-19 06:39:51 -0600

    main rs

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
         "830499a7b3ed234ab617ad6c402c623d0b4318cc4737059c28169e239a25101d",
     );
 
-    let wallet = WalletUnlocked::new_from_private_key(secret.expect("INVALID SECRET"), Some(provider.expect("INVALID PROVIDER")));
+    let wallet = WalletUnlocked::new_from_private_key(secret.expect("INVALID SECRET"), Some(provider));
 
     // // Get the wallet address. Used later with the faucet
     // dbg!(wallet.address().to_string());
